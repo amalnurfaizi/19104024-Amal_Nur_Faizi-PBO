9c9a4896499fa5f38f61e32e1ea0b3fd02d46fc5
amalnurfaizi <72422111+amalnurfaizi@users.noreply.github.com>
2020-11-01 21:02:52 +0700

    Menambahkan Praktikum pada branch Pertemuan4

---
+++ src/com/amalfaizi/Pertemuan4/Praktikum/Abstract/Kucing.java
@@ -0,0 +1,20 @@
+package com.amalfaizi.Pertemuan4.Praktikum.Abstract;
+
+public class Kucing extends Binatang {
+    private String nama;
+
+    public Kucing(String nama) {
+        super("Kucing");
+        this.nama = nama;
+    }
+
+    @Override
+    protected void suara() {
+        System.out.println(" Meonggg");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + nama;
+    }
+}
---
+++ src/com/amalfaizi/Pertemuan4/Praktikum/Interface/InterfaceCD.java
@@ -0,0 +1,7 @@
+package com.amalfaizi.Pertemuan4.Praktikum.Interface;
+
+public interface InterfaceCD {
+    void cetakCD();
+
+    long getHargaCD();
+}
