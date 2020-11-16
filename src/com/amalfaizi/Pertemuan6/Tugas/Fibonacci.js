86782c2d4d11350bd8defea023deb6f1ff67e1f9
amalnurfaizi <72422111+amalnurfaizi@users.noreply.github.com>
2020-11-16 19:39:51 +0700

    Menambahkan Tugas dan Praktikum ke Pertemuan6

---
+++ src/com/amalfaizi/Pertemuan6/Tugas/Fibonacci.js
@@ -0,0 +1,12 @@
+var judul = 'Bilangan Fibonacci = '
+var x = 0
+var y = 1
+var hassil = 0
+
+for (var i = 0; i <= 10; i++){
+    x = x + y
+    x = x + y
+    hasil = hasil + y + ','
+}
+
+console.log(judul + hasil)
