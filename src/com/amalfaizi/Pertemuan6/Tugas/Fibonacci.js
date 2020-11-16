59bfb3b138672ba1f313a9ac4f6a48638e6b9bdb
amalnurfaizi <72422111+amalnurfaizi@users.noreply.github.com>
2020-11-16 19:46:43 +0700

    ubah file

--- src/com/amalfaizi/Pertemuan6/Tugas/Fibonacci.js
+++ src/com/amalfaizi/Pertemuan6/Tugas/Fibonacci.js
@@ -1,11 +1,11 @@
-var judul = 'Bilangan Fibonacci = '
+var judul = "Bilangan Fibonacci = "
 var x = 0
 var y = 1
-var hassil = 0
+var hasil = 0
 
 for (var i = 0; i <= 10; i++){
-    x = x + y
-    x = x + y
+    x = x + y;
+    y = x - y;
     hasil = hasil + y + ','
 }
 
